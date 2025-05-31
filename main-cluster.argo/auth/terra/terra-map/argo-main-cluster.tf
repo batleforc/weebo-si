a3b5e30e1f87360f54b5474286741da9186a8e45
Max batleforc <maxleriche.60@gmail.com>
2025-05-31 18:46:53 +0200

    feat: trigger argo-main-cluster creation

---
+++ main-cluster.argo/auth/terra/terra-map/argo-main-cluster.tf
@@ -0,0 +1,21 @@
+data "authentik_flow" "default-authorization-flow" {
+  slug = "default-provider-authorization-implicit-consent"
+}
+
+resource "authentik_provider_oauth2" "argo-main-cluster" {
+  name               = "main-cluster.argo"
+  client_id          = "main-cluster.argo"
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict",
+      url           = "https://main-cluster.argo.weebo.poc/oauth2/callback",
+    },
+  ]
+}
+
+resource "authentik_application" "argo-main-cluster" {
+  name              = "main-cluster.argo"
+  slug              = "main-cluster.argo"
+  protocol_provider = authentik_provider_oauth2.argo-main-cluster.id
+}
