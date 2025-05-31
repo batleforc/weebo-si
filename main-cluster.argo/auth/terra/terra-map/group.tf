a13280d25c614cc889be8ccf179b4beec6377b14
Max batleforc <maxleriche.60@gmail.com>
2025-05-31 18:37:44 +0200

    fix: wront param

--- main-cluster.argo/auth/terra/terra-map/group.tf
+++ main-cluster.argo/auth/terra/terra-map/group.tf
@@ -1,11 +1,11 @@
 resource "authentik_group" "weebo_user" {
-  name      = "weebo_user"
-  superuser = false
+  name         = "weebo_user"
+  is_superuser = false
 }
 
 
 resource "authentik_group" "weebo_admin" {
-  name      = "weebo_admin"
-  superuser = true
-  parents   = authentik_group.weebo_user.id
+  name         = "weebo_admin"
+  is_superuser = true
+  parent       = authentik_group.weebo_user.id
 }
