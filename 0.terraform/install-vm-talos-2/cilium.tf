c2c05b8b07373fc6f68b4a589d1af2f40d18f543
Max batleforc <maxleriche.60@gmail.com>
2025-06-25 13:52:47 +0200

    feat: commit before commming back to work

--- 0.terraform/install-vm-talos-2/cilium.tf
+++ 0.terraform/install-vm-talos-2/cilium.tf
@@ -1,7 +1,28 @@
-resource "cilium" "example" {
+resource "cilium" "install" {
   set = [
     "k8sServiceHost=${data.ovh_dedicated_server.server.ip}"
   ]
   values  = file("${path.module}/cilium-values.yaml")
   version = var.cilium_version
 }
+
+resource "kubernetes_manifest" "ip-pool" {
+  depends_on = [cilium.install]
+  manifest = {
+    "apiVersion" = "cilium.io/v2alpha1"
+    "kind"       = "CiliumLoadBalancerIPPool"
+    "metadata" = {
+      "name" = "ip-pool"
+    }
+    "spec" = {
+      "blocks" = [
+        {
+          "start" = data.ovh_dedicated_server.server.ip
+          "stop"  = data.ovh_dedicated_server.server.ip
+        }
+      ]
+    }
+  }
+}
+
+
