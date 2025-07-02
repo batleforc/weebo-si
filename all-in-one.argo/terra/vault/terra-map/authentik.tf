ea10c8e42d2c8a251b4d2ff821c5987ae4a70314
Max batleforc <maxleriche.60@gmail.com>
2025-07-03 00:15:25 +0200

    feat: wip

--- all-in-one.argo/terra/vault/terra-map/authentik.tf
+++ all-in-one.argo/terra/vault/terra-map/authentik.tf
@@ -39,6 +39,16 @@ resource "random_string" "token" {
   special = true
 }
 
+resource "random_string" "secret_key" {
+  length  = 24
+  special = true
+}
+
+resource "random_string" "postgres_password" {
+  length  = 42
+  special = true
+}
+
 resource "vault_kv_secret_v2" "example" {
   mount = vault_mount.main-cluster-authentik.path
   name  = "main-config"
@@ -47,6 +57,8 @@ resource "vault_kv_secret_v2" "example" {
       AUTHENTIK_BOOTSTRAP_PASSWORD = random_string.password.result,
       AUTHENTIK_BOOTSTRAP_TOKEN    = random_string.token.result,
       AUTHENTIK_BOOTSTRAP_EMAIL    = "admin@weebo.poc"
+      AUTHENTIK_SECRET_KEY         = random_string.secret_key.result,
+      AUTHENTIK_POSTGRES_PASSWORD  = random_string.postgres_password.result,
     }
   )
 }
