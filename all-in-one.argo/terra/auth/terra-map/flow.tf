2238eaed2fbb0f1ead60cbcac2fff2fd7b951fd5
Max batleforc <maxleriche.60@gmail.com>
2025-07-04 01:34:38 +0200

    feat: wip

--- all-in-one.argo/terra/auth/terra-map/flow.tf
+++ all-in-one.argo/terra/auth/terra-map/flow.tf
@@ -11,9 +11,15 @@ data "authentik_brand" "authentik-default" {
 }
 
 resource "authentik_brand" "default" {
-  domain           = "weebo"
-  default          = true
-  flow_device_code = authentik_flow.token-authentik-flow.uuid
-  branding_logo    = data.authentik_brand.authentik-default.branding_logo
-  branding_favicon = data.authentik_brand.authentik-default.branding_favicon
+  domain              = "weebo"
+  default             = true
+  flow_device_code    = authentik_flow.token-authentik-flow.uuid
+  branding_logo       = data.authentik_brand.authentik-default.branding_logo
+  branding_favicon    = data.authentik_brand.authentik-default.branding_favicon
+  default_application = data.authentik_brand.authentik-default.default_application
+  flow_authentication = data.authentik_brand.authentik-default.flow_authentication
+  flow_invalidation   = data.authentik_brand.authentik-default.flow_invalidation
+  flow_recovery       = data.authentik_brand.authentik-default.flow_recovery
+  flow_unenrollment   = data.authentik_brand.authentik-default.flow_unenrollment
+  flow_user_settings  = data.authentik_brand.authentik-default.flow_user_settings
 }
