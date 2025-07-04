3a745a80120655a14851d595f702697b48ae6994
Max batleforc <maxleriche.60@gmail.com>
2025-07-04 23:09:17 +0200

    feat: try to set background

--- all-in-one.argo/terra/auth/terra-map/flow.tf
+++ all-in-one.argo/terra/auth/terra-map/flow.tf
@@ -11,16 +11,17 @@ data "authentik_brand" "authentik-default" {
 }
 
 resource "authentik_brand" "default" {
-  domain              = "weebo"
-  default             = true
-  branding_title      = "Weebo Authentik"
-  flow_device_code    = authentik_flow.token-authentik-flow.uuid
-  branding_logo       = data.authentik_brand.authentik-default.branding_logo
-  branding_favicon    = data.authentik_brand.authentik-default.branding_favicon
-  default_application = data.authentik_brand.authentik-default.default_application
-  flow_authentication = data.authentik_brand.authentik-default.flow_authentication
-  flow_invalidation   = data.authentik_brand.authentik-default.flow_invalidation
-  flow_recovery       = data.authentik_brand.authentik-default.flow_recovery
-  flow_unenrollment   = data.authentik_brand.authentik-default.flow_unenrollment
-  flow_user_settings  = data.authentik_brand.authentik-default.flow_user_settings
+  domain                           = "weebo"
+  default                          = true
+  branding_title                   = "Weebo Authentik"
+  flow_device_code                 = authentik_flow.token-authentik-flow.uuid
+  branding_logo                    = data.authentik_brand.authentik-default.branding_logo
+  branding_favicon                 = data.authentik_brand.authentik-default.branding_favicon
+  branding_default_flow_background = "https://batleforc.github.io/weebo-si/banner.png"
+  default_application              = data.authentik_brand.authentik-default.default_application
+  flow_authentication              = data.authentik_brand.authentik-default.flow_authentication
+  flow_invalidation                = data.authentik_brand.authentik-default.flow_invalidation
+  flow_recovery                    = data.authentik_brand.authentik-default.flow_recovery
+  flow_unenrollment                = data.authentik_brand.authentik-default.flow_unenrollment
+  flow_user_settings               = data.authentik_brand.authentik-default.flow_user_settings
 }
