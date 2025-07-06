2a7929ba38c63c769ac5f03ab75547a25aded1cc
Max batleforc <maxleriche.60@gmail.com>
2025-07-07 01:40:02 +0200

    feat: wip

--- all-in-one.argo/terra/netbird/terra-map/main.tf
+++ all-in-one.argo/terra/netbird/terra-map/main.tf
@@ -21,7 +21,7 @@ variable "netbird_token" {
 
 variable "netbird_management_url" {
   description = "NetBird Management URL"
-  default     = "https://netbird.4.weebo.fr/api"
+  default     = "https://netbird.4.weebo.fr"
 }
 
 provider "vault" {
