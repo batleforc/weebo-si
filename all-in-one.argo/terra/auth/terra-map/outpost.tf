f92bf5971fe4bb633387a01d6c10b86d99e4aa0e
Max batleforc <maxleriche.60@gmail.com>
2025-08-06 00:56:03 +0200

    feat: wip

--- all-in-one.argo/terra/auth/terra-map/outpost.tf
+++ all-in-one.argo/terra/auth/terra-map/outpost.tf
@@ -1,6 +1,6 @@
 resource "authentik_outpost" "outpost" {
   name = "authentik Embedded Outpost"
   protocol_providers = [
-    authentik_provider_proxy.kubevirt.id
+    authentik_provider_proxy.kubevirt-manager.id
   ]
 }
