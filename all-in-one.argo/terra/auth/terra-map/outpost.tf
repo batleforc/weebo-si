5c649a7155aaf0494f1fc3ef56514ab897f20d53
Max batleforc <maxleriche.60@gmail.com>
2025-08-06 01:09:53 +0200

    feat: wip

--- all-in-one.argo/terra/auth/terra-map/outpost.tf
+++ all-in-one.argo/terra/auth/terra-map/outpost.tf
@@ -1,6 +1,10 @@
-resource "authentik_outpost" "outpost" {
-  name = "authentik Embedded Outpost"
-  protocol_providers = [
-    authentik_provider_proxy.kubevirt-manager.id
-  ]
-}
+# resource "authentik_outpost" "outpost" {
+#   name = "authentik Embedded Outpost"
+#   protocol_providers = [
+#     authentik_provider_proxy.kubevirt-manager.id
+#   ]
+# }
+# See https://github.com/DCCoder90/home-net/issues/12
+# will add manually ATM or via rest API
+
+
