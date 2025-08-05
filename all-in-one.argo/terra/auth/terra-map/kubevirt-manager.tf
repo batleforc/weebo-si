8ea9c96120c3dee196bf0b47ae960bf312ced33d
Max batleforc <maxleriche.60@gmail.com>
2025-08-06 00:45:45 +0200

    feat: add invalidation_flow

--- all-in-one.argo/terra/auth/terra-map/kubevirt-manager.tf
+++ all-in-one.argo/terra/auth/terra-map/kubevirt-manager.tf
@@ -3,6 +3,7 @@ resource "authentik_provider_proxy" "kubevirt-manager" {
   internal_host      = "http://kubevirt-manager.kubevirt-manager.svc.cluster.local:8080"
   external_host      = "https://kubevirt-manager.4.weebo.fr"
   authorization_flow = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.default-invalidation-flow.id
 }
 
 resource "authentik_application" "kubevirt-manager" {
