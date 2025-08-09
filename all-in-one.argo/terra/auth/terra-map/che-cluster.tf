776926c779f8399630cc84a70724f552ed74c501
Max batleforc <maxleriche.60@gmail.com>
2025-08-09 23:58:56 +0200

    feat: wip

---
+++ all-in-one.argo/terra/auth/terra-map/che-cluster.tf
@@ -0,0 +1,45 @@
+resource "authentik_provider_oauth2" "che" {
+  name               = "che-cluster"
+  client_id          = "che-cluster"
+  sub_mode           = "user_email"
+  invalidation_flow  = data.authentik_flow.default-invalidation-flow.id
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  signing_key        = data.authentik_certificate_key_pair.generated.id
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict",
+      url           = "http://localhost:8000",
+    },
+    {
+      matching_mode = "strict",
+      url           = "https://che.dev.4.weebo.fr",
+    },
+    {
+      matching_mode = "regex",
+      url           = "https://.*.che.dev.4.weebo.fr",
+    },
+  ]
+  property_mappings = [
+    data.authentik_property_mapping_provider_scope.scope-email.id,
+    data.authentik_property_mapping_provider_scope.scope-profile.id,
+    data.authentik_property_mapping_provider_scope.scope-openid.id,
+  ]
+}
+
+resource "authentik_application" "che" {
+  name              = "che-cluster"
+  slug              = "che-cluster"
+  protocol_provider = authentik_provider_oauth2.che.id
+}
+
+resource "vault_kv_secret_v2" "che" {
+  mount = "mc-authentik"
+  name  = "che/auth"
+  data_json = jsonencode(
+    {
+      AUTHENTIK_CLIENT_ID     = authentik_provider_oauth2.che.client_id,
+      AUTHENTIK_CLIENT_SECRET = authentik_provider_oauth2.che.client_secret,
+      AUTHENTIK_URL           = "https://login.4.weebo.fr/application/o/${authentik_application.che.slug}/",
+    }
+  )
+}
---
+++ all-in-one.argo/terra/netbird/terra-map/che-cluster.tf
@@ -0,0 +1,53 @@
+resource "netbird_network" "che-cluster" {
+  name        = "che-cluster"
+  description = "che-cluster network"
+}
+
+resource "netbird_group" "che-cluster-peer" {
+  name = "che-cluster-peer"
+}
+
+resource "netbird_setup_key" "che-cluster-peer" {
+  name                   = "che-cluster-peer"
+  expiry_seconds         = 0 # 30 days
+  type                   = "reusable"
+  allow_extra_dns_labels = true
+  auto_groups            = [netbird_group.che-cluster-peer.id]
+  ephemeral              = true
+  revoked                = false
+  usage_limit            = 0
+}
+
+resource "vault_kv_secret_v2" "netbird" {
+  mount = "mc-authentik"
+  name  = "che/sub"
+  data_json = jsonencode(
+    {
+      KUBERNETES_SETUP_KEY = netbird_setup_key.che-cluster-peer.key
+    }
+  )
+}
+
+resource "netbird_network_resource" "che-cluster-pod-cidr" {
+  name        = "che-cluster-pod-cidr"
+  network_id  = netbird_network.che-cluster.id
+  groups      = [netbird_group.che-cluster-peer.id]
+  address     = "10.245.0.0/16"
+  description = "Che cluster pod's CIDR"
+}
+
+resource "netbird_network_resource" "che-cluster-service-cidr" {
+  name        = "che-cluster-service-cidr"
+  network_id  = netbird_network.che-cluster.id
+  groups      = [netbird_group.che-cluster-peer.id]
+  address     = "15.112.0.0/12"
+  description = "Che cluster service's CIDR"
+}
+
+resource "netbird_network_resource" "che-cluster-fqdn" {
+  name        = "che-cluster-fqdn"
+  network_id  = netbird_network.che-cluster.id
+  groups      = [netbird_group.che-cluster-peer.id]
+  address     = "*.che.cluster.4.weebo.fr"
+  description = "Che cluster FQDN's CIDR"
+}
