0d9c5811693cba48a19767efd5ae190bbcec3f56
Max batleforc <maxleriche.60@gmail.com>
2025-08-16 01:25:03 +0200

    feat: create new subspace in mc authentik

--- all-in-one.argo/terra/vault/terra-map/authentik.tf
+++ all-in-one.argo/terra/vault/terra-map/authentik.tf
@@ -38,6 +38,25 @@ path "mc-authentik/metadata/{{identity.entity.aliases.auth_kubernetes_225a14d3.m
 EOT
 }
 
+resource "vault_policy" "authentik_reader_policy_che_cluster" {
+  name = "authentik_reader_policy_che_cluster"
+
+  policy = <<EOT
+path "mc-authentik/data/che-cluster/{{identity.entity.aliases.auth_kubernetes_cd5c654a.metadata.service_account_namespace}}/*" {
+  capabilities = ["read","list"]
+}
+path "mc-authentik/data/che-cluster/{{identity.entity.aliases.auth_kubernetes_cd5c654a.metadata.service_account_namespace}}/config" {
+  capabilities = ["create", "read", "update", "delete", "list"]
+}
+path "mc-authentik/data/che-cluster/{{identity.entity.aliases.auth_kubernetes_cd5c654a.metadata.service_account_namespace}}/sub" {
+  capabilities = ["create", "read", "update", "delete", "list"]
+}
+path "mc-authentik/metadata/che-cluster/{{identity.entity.aliases.auth_kubernetes_cd5c654a.metadata.service_account_namespace}}/sub" {
+  capabilities = ["read","list"]
+}
+EOT
+}
+
 resource "vault_policy" "authentik_vpn_policy" {
   name = "authentik_vpn_policy"
 
@@ -125,5 +144,12 @@ resource "vault_kubernetes_auth_backend_role" "auth-vpn" {
   bound_service_account_namespaces = ["netbird"]
   token_ttl                        = 3600
   token_policies                   = [vault_policy.authentik_vpn_policy.name]
+}
 
+resource "vault_kubernetes_auth_backend_role" "auth-reader-che-cluster" {
+  role_name                        = "auth-reader-che-cluster"
+  bound_service_account_names      = ["che", "default"]
+  bound_service_account_namespaces = ["eclipse-che"]
+  token_ttl                        = 3600
+  token_policies                   = [vault_policy.authentik_reader_policy_che_cluster.name]
 }
