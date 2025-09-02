7d1f54d85cf84b4ff394d441cb83ec54720e1dbb
Max Batleforc <maxleriche.60@gmail.com>
2025-09-03 00:11:58 +0200

    feat(monitoring): ...

--- all-in-one.argo/terra/vault/terra-map/auth-monitoring.tf
+++ all-in-one.argo/terra/vault/terra-map/auth-monitoring.tf
@@ -2,9 +2,6 @@ resource "vault_policy" "authentik_monitoring" {
   name = "authentik_monitoring"
 
   policy = <<EOT
-path "mc-authentik/data/che-cluster/{{identity.entity.aliases.auth_kubernetes_225a14d3.metadata.service_account_namespace}}/*" {
-  capabilities = ["read","list"]
-}
 path "mc-authentik/data/che-cluster/{{identity.entity.aliases.auth_kubernetes_225a14d3.metadata.service_account_namespace}}/config" {
   capabilities = ["create", "read", "update", "delete", "list","patch"]
 }
@@ -43,7 +40,9 @@ path "mc-authentik/metadata/+/monitoring" {
 path "mc-authentik/data/+/monitoring" {
   capabilities = ["create", "read", "update", "delete", "list","patch"]
 }
-
+path "mc-authentik/data/che-cluster/{{identity.entity.aliases.auth_kubernetes_225a14d3.metadata.service_account_namespace}}/*" {
+  capabilities = ["read","list"]
+}
 EOT
 }
 
