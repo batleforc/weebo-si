8b415c5371d159b854fd3bfb1e243ca2b99d806a
Max Batleforc <maxleriche.60@gmail.com>
2025-09-06 18:10:57 +0200

    feat(vault): allow grafana ns to fetch it's own value

--- all-in-one.argo/terra/vault/terra-map/auth-base.tf
+++ all-in-one.argo/terra/vault/terra-map/auth-base.tf
@@ -49,7 +49,7 @@ resource "vault_kubernetes_auth_backend_role" "auth-write" {
 resource "vault_kubernetes_auth_backend_role" "auth-read" {
   role_name                        = "auth-read"
   bound_service_account_names      = ["authentik", "default", "kubevirt-omni"]
-  bound_service_account_namespaces = ["authentik", "argocd", "netbird", "kubevirt-omni", "che"]
+  bound_service_account_namespaces = ["authentik", "argocd", "netbird", "kubevirt-omni", "che","grafana"]
   token_ttl                        = 3600
   token_policies                   = [vault_policy.authentik_reader_policy.name]
 }
