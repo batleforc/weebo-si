6e9d6a16e99d3fc13d86cf38e405bd5f50a2d38b
Max Batleforc <maxleriche.60@gmail.com>
2025-09-13 17:29:32 +0200

    feat(harbor): add ns to vault auth-read

--- all-in-one.argo/terra/vault/terra-map/auth-base.tf
+++ all-in-one.argo/terra/vault/terra-map/auth-base.tf
@@ -55,7 +55,7 @@ resource "vault_kubernetes_auth_backend_role" "auth-write" {
 resource "vault_kubernetes_auth_backend_role" "auth-read" {
   role_name                        = "auth-read"
   bound_service_account_names      = ["authentik", "default", "kubevirt-omni"]
-  bound_service_account_namespaces = ["authentik", "argocd", "netbird", "kubevirt-omni", "che","grafana"]
+  bound_service_account_namespaces = ["authentik", "argocd", "netbird", "kubevirt-omni", "che","grafana","harbor"]
   token_ttl                        = 3600
   token_policies                   = [vault_policy.authentik_reader_policy.name]
 }
