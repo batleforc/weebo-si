ef8ece4cdd5faab0b7093574a256157893aa8f8d
Max Batleforc <maxleriche.60@gmail.com>
2025-09-17 22:27:05 +0200

    feat(harbor): create auth

---
+++ all-in-one.argo/terra/auth/terra-map/harbor.tf
@@ -0,0 +1,37 @@
+resource "authentik_provider_oauth2" "harbor" {
+  name               = "harbor"
+  client_id          = "harbor"
+  invalidation_flow  = data.authentik_flow.default-invalidation-flow.id
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  signing_key        = data.authentik_certificate_key_pair.generated.id
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict",
+      url           = "https://harbor.4.weebo.fr/c/oidc/callback/",
+    },
+  ]
+  property_mappings = [
+    data.authentik_property_mapping_provider_scope.scope-email.id,
+    data.authentik_property_mapping_provider_scope.scope-profile.id,
+    data.authentik_property_mapping_provider_scope.scope-openid.id,
+    data.authentik_property_mapping_provider_scope.scope-offline.id,
+  ]
+}
+
+resource "authentik_application" "harbor" {
+  name              = "harbor"
+  slug              = "harbor"
+  protocol_provider = authentik_provider_oauth2.harbor.id
+}
+
+resource "vault_kv_secret_v2" "harbor" {
+  mount = "mc-authentik"
+  name  = "harbor/auth"
+  data_json = jsonencode(
+    {
+      AUTHENTIK_CLIENT_ID     = authentik_provider_oauth2.harbor.client_id,
+      AUTHENTIK_CLIENT_SECRET = authentik_provider_oauth2.harbor.client_secret,
+      AUTHENTIK_URL           = "https://login.4.weebo.fr/application/o/${authentik_application.harbor.slug}/",
+    }
+  )
+}
--- all-in-one.argo/terra/harbor/terra-map/main.tf
+++ all-in-one.argo/terra/harbor/terra-map/main.tf
@@ -1,51 +1,16 @@
 terraform {
   required_providers {
-    netbird = {
-      source = "registry.terraform.io/netbirdio/netbird"
+    harbor = {
+      source = "goharbor/harbor"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "5.0.0"
     }
   }
 }
 
-locals {
-  token_vault = file("/var/run/secrets/kubernetes.io/serviceaccount/token")
-}
-
-variable "netbird_token" {
-  sensitive   = true
-  description = "NetBird Management Access Token"
-}
-
-variable "netbird_management_url" {
-  description = "NetBird Management URL"
-  default     = "https://netbird.4.weebo.fr"
-}
-
-provider "vault" {
-  address          = "https://vault.vault:8200"
-  ca_cert_file     = "/etc/ssl/vault/ca.crt"
-  skip_child_token = "true"
-  auth_login_jwt {
-    role  = "auth-vpn"
-    jwt   = local.token_vault
-    mount = "kubernetes"
-  }
-}
-
-
-
-provider "netbird" {
-  token          = var.netbird_token
-  management_url = var.netbird_management_url
-}
-
-
-resource "netbird_account_settings" "example" {
-  jwt_allow_groups           = []
-  jwt_groups_claim_name      = "groups"
-  groups_propagation_enabled = true
-  jwt_groups_enabled         = true
+provider "harbor" {
+  url      = "https://harbor.4.weebo.fr"
+  username = var.username
+  password = var.password
 }
