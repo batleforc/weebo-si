93bc9e22291532a92ca8c18d3bf0e2de8520bd6d
Max Batleforc <maxleriche.60@gmail.com>
2025-10-11 18:09:18 +0200

    feat: wip add all group to max

--- all-in-one.argo/terra/auth/terra-map/user.tf
+++ all-in-one.argo/terra/auth/terra-map/user.tf
@@ -1,7 +1,7 @@
 resource "authentik_user" "batleforc" {
   username  = "batleforc"
   name      = "Batleforc"
-  groups    = [authentik_group.weebo_admin.id]
+  groups    = [authentik_group.*.id]
   is_active = true
   email     = "batleforc@weebo.poc"
 }
