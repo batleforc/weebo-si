0de1ed85a469cc3cef491e90d15e1aae547d5b10
Max Batleforc <maxleriche.60@gmail.com>
2026-06-16 22:29:04 +0200

    feat: wip

--- 2.terra/vault/terra-map/auth-policy.tf
+++
@@ -1,7 +0,0 @@
-resource "vault_policy" "authentik_policy_weebo_admin" {
-  name = "weebo_admin"
-
-  policy = <<EOT
-path "*" { capabilities = ["read","list", "update", "create", "delete", "sudo"] }
-EOT
-}
