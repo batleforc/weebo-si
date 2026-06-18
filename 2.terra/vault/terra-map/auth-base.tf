918da6a3c038640d93e4a0541427e7e2d7464ac1
Max Batleforc <maxleriche.60@gmail.com>
2026-06-18 23:17:21 +0200

    feat: wip

--- 2.terra/vault/terra-map/auth-base.tf
+++ 2.terra/vault/terra-map/auth-base.tf
@@ -74,7 +74,7 @@ resource "vault_kubernetes_auth_backend_role" "auth-write" {
 resource "vault_kubernetes_auth_backend_role" "auth-read" {
   role_name                        = "auth-read"
   bound_service_account_names      = ["authentik", "default", "vaultwarden-svc"]
-  bound_service_account_namespaces = ["auth", "argocd"]
+  bound_service_account_namespaces = ["auth", "argocd", "dex"]
   token_ttl                        = 3600
   token_policies                   = [vault_policy.mv_reader_policy.name]
 }
