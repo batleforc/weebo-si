8e2d3eb8966f9f8939d60b079b6f48336f8f5d93
Max Batleforc <maxleriche.60@gmail.com>
2026-06-24 21:27:14 +0200

    feat: wip

--- 2.terra/vault/terra-map/pki.tf
+++ 2.terra/vault/terra-map/pki.tf
@@ -1,3 +1,4 @@
+# https://developer.hashicorp.com/vault/tutorials/pki/pki-engine?productSlug=vault&tutorialSlug=secrets-management&tutorialSlug=pki-engine
 variable "pki" {
   type    = string
   default = "pki"
@@ -13,6 +14,31 @@ data "vault_pki_secret_backend_issuer" "root_issuer" {
   issuer_ref = keys({ for k, v in data.vault_pki_secret_backend_issuers.pki_issuers.key_info : k => k if jsondecode(v).is_default == true })[0]
 }
 
-output "outtruc" {
-  value = keys({ for k, v in data.vault_pki_secret_backend_issuers.pki_issuers.key_info : k => k if jsondecode(v).is_default == true })[0]
+resource "vault_mount" "pki_int" {
+  path        = "pki_int"
+  type        = "pki"
+  description = "Intermediary pki cert"
+
+  default_lease_ttl_seconds = 86400
+  max_lease_ttl_seconds     = 157680000
+}
+
+resource "vault_pki_secret_backend_intermediate_cert_request" "csr-request" {
+  backend     = vault_mount.pki_int.path
+  type        = "internal"
+  common_name = "weebo.poc Intermediate Authority"
+}
+
+resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
+  backend     = var.pki
+  common_name = "weebo.poc Intermediate Authority"
+  csr         = vault_pki_secret_backend_intermediate_cert_request.csr-request.csr
+  format      = "pem_bundle"
+  ttl         = 15480000
+  issuer_ref  = data.vault_pki_secret_backend_issuer.root_issuer.issuer_ref
+}
+
+resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
+  backend     = var.pki
+  certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
 }
