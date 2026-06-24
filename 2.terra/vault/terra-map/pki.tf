f074f270dc4ac931ebf40f30d3b82911e4f2115f
Max Batleforc <maxleriche.60@gmail.com>
2026-06-24 20:58:19 +0200

    feat: wip

--- 2.terra/vault/terra-map/pki.tf
+++ 2.terra/vault/terra-map/pki.tf
@@ -7,7 +7,11 @@ data "vault_pki_secret_backend_issuers" "pki_issuers" {
   backend = var.pki
 }
 
-data "vault_pki_secret_backend_issuer" "root_issuer" {
-  backend    = var.pki
-  issuer_ref = data.vault_pki_secret_backend_issuers.pki_issuers.key_info[0]
+# data "vault_pki_secret_backend_issuer" "root_issuer" {
+#   backend    = var.pki
+#   issuer_ref = data.vault_pki_secret_backend_issuers.pki_issuers.key_info[0]
+# }
+
+output "outtruc" {
+  value = data.vault_pki_secret_backend_issuers.pki_issuers.key_info
 }
