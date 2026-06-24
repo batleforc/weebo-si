5a9de6069b9fe784f985036235033c9e8de6069e
Max Batleforc <maxleriche.60@gmail.com>
2026-06-24 21:01:52 +0200

    feat: wip

--- 2.terra/vault/terra-map/pki.tf
+++ 2.terra/vault/terra-map/pki.tf
@@ -7,10 +7,11 @@ data "vault_pki_secret_backend_issuers" "pki_issuers" {
   backend = var.pki
 }
 
-# data "vault_pki_secret_backend_issuer" "root_issuer" {
-#   backend    = var.pki
-#   issuer_ref = data.vault_pki_secret_backend_issuers.pki_issuers.key_info[0]
-# }
+data "vault_pki_secret_backend_issuer" "root_issuer" {
+  backend = var.pki
+  # Get the one who is default
+  issuer_ref = { for k, v in data.vault_pki_secret_backend_issuers.pki_issuers.key_info : k => v if v.is_default == true }
+}
 
 output "outtruc" {
   value = data.vault_pki_secret_backend_issuers.pki_issuers.key_info
