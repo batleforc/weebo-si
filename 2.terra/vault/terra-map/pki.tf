08826ad42ff23abe1292d4325641b37f930edd5f
Max Batleforc <maxleriche.60@gmail.com>
2026-06-26 15:34:00 +0200

    feat: wip

--- 2.terra/vault/terra-map/pki.tf
+++ 2.terra/vault/terra-map/pki.tf
@@ -14,55 +14,55 @@ data "vault_pki_secret_backend_issuer" "root_issuer" {
   issuer_ref = keys({ for k, v in data.vault_pki_secret_backend_issuers.pki_issuers.key_info : k => k if jsondecode(v).is_default == true })[0]
 }
 
-resource "vault_mount" "pki_int" {
-  path        = "pki_int"
-  type        = "pki"
-  description = "Intermediary pki cert"
+# resource "vault_mount" "pki_int" {
+#   path        = "pki_int"
+#   type        = "pki"
+#   description = "Intermediary pki cert"
 
-  default_lease_ttl_seconds = 86400
-  max_lease_ttl_seconds     = 157680000
-}
+#   default_lease_ttl_seconds = 86400
+#   max_lease_ttl_seconds     = 157680000
+# }
 
-resource "vault_pki_secret_backend_intermediate_cert_request" "csr-request" {
-  backend     = vault_mount.pki_int.path
-  type        = "internal"
-  common_name = "weebo.poc Intermediate Authority"
-  key_bits    = 4096
-}
+# resource "vault_pki_secret_backend_intermediate_cert_request" "csr-request" {
+#   backend     = vault_mount.pki_int.path
+#   type        = "internal"
+#   common_name = "weebo.poc Intermediate Authority"
+#   key_bits    = 4096
+# }
 
-resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
-  backend     = var.pki
-  common_name = "weebo.poc Intermediate Authority"
-  csr         = vault_pki_secret_backend_intermediate_cert_request.csr-request.csr
-  format      = "pem_bundle"
-  ttl         = 15480000
-  issuer_ref  = data.vault_pki_secret_backend_issuer.root_issuer.issuer_ref
-}
+# resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
+#   backend     = var.pki
+#   common_name = "weebo.poc Intermediate Authority"
+#   csr         = vault_pki_secret_backend_intermediate_cert_request.csr-request.csr
+#   format      = "pem_bundle"
+#   ttl         = 15480000
+#   issuer_ref  = data.vault_pki_secret_backend_issuer.root_issuer.issuer_ref
+# }
 
-resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
-  backend     = vault_mount.pki_int.path
-  certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
-}
+# resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
+#   backend     = vault_mount.pki_int.path
+#   certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
+# }
 
-data "vault_pki_secret_backend_issuers" "pki_intermediate_issuers" {
-  backend = vault_mount.pki_int.path
-}
+# data "vault_pki_secret_backend_issuers" "pki_intermediate_issuers" {
+#   backend = vault_mount.pki_int.path
+# }
 
-data "vault_pki_secret_backend_issuer" "intermediate" {
-  backend = vault_mount.pki_int.path
-  # Get the one who is default
-  issuer_ref = keys({ for k, v in data.vault_pki_secret_backend_issuers.pki_intermediate_issuers.key_info : k => k if jsondecode(v).is_default == true })[0]
-}
+# data "vault_pki_secret_backend_issuer" "intermediate" {
+#   backend = vault_mount.pki_int.path
+#   # Get the one who is default
+#   issuer_ref = keys({ for k, v in data.vault_pki_secret_backend_issuers.pki_intermediate_issuers.key_info : k => k if jsondecode(v).is_default == true })[0]
+# }
 
-resource "vault_pki_secret_backend_role" "intermediate_role" {
-  backend          = vault_mount.pki_int.path
-  issuer_ref       = data.vault_pki_secret_backend_issuer.intermediate.issuer_ref
-  name             = "weebo-poc"
-  ttl              = 86400
-  max_ttl          = 2592000
-  allow_ip_sans    = true
-  key_type         = "rsa"
-  key_bits         = 4096
-  allowed_domains  = ["weebo.poc"]
-  allow_subdomains = true
-}
+# resource "vault_pki_secret_backend_role" "intermediate_role" {
+#   backend          = vault_mount.pki_int.path
+#   issuer_ref       = data.vault_pki_secret_backend_issuer.intermediate.issuer_ref
+#   name             = "weebo-poc"
+#   ttl              = 86400
+#   max_ttl          = 2592000
+#   allow_ip_sans    = true
+#   key_type         = "rsa"
+#   key_bits         = 4096
+#   allowed_domains  = ["weebo.poc"]
+#   allow_subdomains = true
+# }
