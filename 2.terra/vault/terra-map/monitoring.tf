be206528f54c18f787314ff55c62268360d17c34
Max Batleforc <maxleriche.60@gmail.com>
2026-07-09 02:07:38 +0200

    feat: wip

--- 2.terra/vault/terra-map/monitoring.tf
+++ 2.terra/vault/terra-map/monitoring.tf
@@ -3,12 +3,24 @@ resource "random_string" "monitoring-clickhouse-admin-password" {
   special = true
 }
 
+resource "random_string" "monitoring-hyperdx-api-key" {
+  length  = 42
+  special = true
+}
+
+resource "random_string" "monitoring-clickhouse-app-password" {
+  length  = 42
+  special = true
+}
+
 resource "vault_kv_secret_v2" "monitoring-clickhouse" {
   mount = "mv"
   name  = "monitoring/config"
   data_json = jsonencode(
     {
       CLICKHOUSE_ADMIN_PASSWORD = random_string.monitoring-clickhouse-admin-password.result
+      HYPERDX_API_KEY           = random_string.monitoring-hyperdx-api-key.result
+      CLICKHOUSE_APP_PASSWORD   = random_string.monitoring-clickhouse-app-password.result
     }
   )
 }
